8656777a7dcd16aa99695ea2802d1cc1c912e49f
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-14 16:44:57 -0500

    Primer paso para el Programa Mascota con clase y metodo

---
+++ Mascota/Mascota.java
@@ -0,0 +1,10 @@
+public class Mascota {
+    String nombre;
+    String raza;
+    int edad;
+    void mostrarMascota(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Raza: " + raza);
+        System.out.println("Edad: " + edad);
+    }
+}
