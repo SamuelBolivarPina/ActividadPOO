7bff83ed9a9ec1e3b9f915ed741c69a339c75d6b
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-14 16:34:17 -0500

    Primer paso para el programa Producto con sus clase y metodo

---
+++ Producto/Producto.java
@@ -0,0 +1,10 @@
+public class Producto {
+    String nombre;
+    double precio;
+    int stock;
+    void mostrarProducto(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: " + precio);
+        System.out.println("Stock: " + stock);
+    }
+}
