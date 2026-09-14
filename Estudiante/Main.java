ffd6137ab5196f0598507eda2e3ded4d7c4ca3f2
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-14 16:52:31 -0500

    Creando el primer objeto para Estudiante con el metodo main

---
+++ Estudiante/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Asignación manual de datos sin usar 'new' ni instancias de la clase
+        String nombre = "Samuel";
+        String codigo = "7502610043";
+        int semestre = 2;
+
+        // Mostrar la información
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Código: " + codigo);
+        System.out.println("Semestre: " + semestre);
+    }
+}
