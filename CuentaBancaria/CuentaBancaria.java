cebf2579070547e38891f96c08d5c7acf34fd358
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-14 16:40:45 -0500

    Primer paso para el programa CuentaBancaria con clase y metodo

---
+++ CuentaBancaria/CuentaBancaria.java
@@ -0,0 +1,10 @@
+public class CuentaBancaria {
+    String titular;
+    String numero;
+    double saldo;
+    void mostrarCuenta(){
+        System.out.println("Titular: " + titular);
+        System.out.println("numero: " + numero);
+        System.out.println("Saldo" + saldo);
+    }
+}
