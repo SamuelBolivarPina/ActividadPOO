8a31cad7d0f2a3a685774f7fd88dfb9bde9efba1
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-14 16:14:27 -0500

    Primer paso del programa estudiante con clase y metodo

---
+++ Estudiante/Estudiante.java
@@ -0,0 +1,10 @@
+public class Estudiante {
+    String nombre;
+    String codigo;
+    int semestre;
+    void mostrarInfo(){
+        System.out.println("Nombre :" + nombre);
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Semestre" + semestre);
+    }
+}
