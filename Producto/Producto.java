5b7586af4629a5bc867f4dd884d82ec611e5367c
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-14 22:04:36 -0500

    elaboracion de constructores de vacio y parametros en el programa Producto

--- Producto/Producto.java
+++ Producto/Producto.java
@@ -2,6 +2,14 @@ public class Producto {
     String nombre;
     double precio;
     int stock;
+
+    public Producto(){
+    }
+    public Producto(String nombre, double precio, int stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
     void mostrarProducto(){
         System.out.println("Nombre: " + nombre);
         System.out.println("Precio: " + precio);
