b2e0efa43b02071b66dee66c7f56dd13ef6f711d
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-14 21:32:24 -0500

    elaboracion de la clase Libro

---
+++ Libro/Libro.java
@@ -0,0 +1,9 @@
+public class Libro {
+    String titulo;
+    String autor;
+    int paginas;
+
+    public void mostrarInfo() {
+        System.out.println("Título: " + titulo + " | Autor: " + autor + " | Páginas: " + paginas);
+    }
+}
