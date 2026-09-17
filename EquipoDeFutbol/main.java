e8f4422045fedb9a984347f6261ed27e878edcbc
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-16 20:32:49 -0500

    elaboracion de objetos/ahora si no hay nada raro...por ahora

---
+++ EquipoDeFutbol/main.java
@@ -0,0 +1,27 @@
+package EquipoDeFutbol;
+
+public class main {
+
+    public static int ganarPartido(int puntosActuales) {
+        return puntosActuales + 3;
+    }
+
+    public static void mostrarEstadisticas(String nombre, String ciudad, int puntos) {
+        System.out.println("Equipo: " + nombre + " | Ciudad: " + ciudad + " | Puntos: " + puntos);
+    }
+
+    public static void main(String[] args) {
+
+        String nombre1 = "Real Cartagena";
+        String ciudad1 = "Cartagena";
+        int puntos1 = 24;
+
+        System.out.println("Tabla de Posiciones Inicial");
+        mostrarEstadisticas(nombre1, ciudad1, puntos1);
+
+        puntos1 = ganarPartido(puntos1);
+
+        System.out.println("Después de ganar un partido (+3 pts)");
+        mostrarEstadisticas(nombre1, ciudad1, puntos1);
+    }
+}
