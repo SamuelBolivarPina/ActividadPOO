f498b12c2bc1e2e5ee67d35d9aa4b3c96b5b239e
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 17:03:21 -0500

    colocando los constructores para crear los objetos de Samuel

---
+++ src/Samuel/main.java
@@ -0,0 +1,15 @@
+package Samuel;
+
+public class main {
+    String nombre;
+    int edad;
+    String universidad;
+    String carrera;
+
+    public Samuel(String nombre, int edad, String universidad, String carrera) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.universidad = universidad;
+        this.carrera = carrera;
+    }
+}
