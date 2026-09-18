828a3244e407b42513471076229a14736e2347d2
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 16:06:47 -0500

    elaboracion de constructores

---
+++ CanalStream/CanalStream.java
@@ -0,0 +1,13 @@
+public class CanalStream {
+    String nombreCanal;
+    String plataforma;
+    double horasTransmitidas;
+    public CanalStream() {
+    }
+
+    public CanalStream(String nombreCanal, String plataforma, double horasTransmitidas) {
+        this.nombreCanal = nombreCanal;
+        this.plataforma = plataforma;
+        this.horasTransmitidas = horasTransmitidas;
+    }
+}
