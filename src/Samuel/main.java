f007cb2f51c89147923637e164b6120bc1855879
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 17:06:23 -0500

    elaboracion de los objetos

--- src/Samuel/main.java
+++ src/Samuel/main.java
@@ -5,4 +5,8 @@ public class main {
     public static int cumplirAnioProc(int edadActual) {
         return edadActual + 1;
     }
+    public static void mostrarPerfilProc(String nombre, int edad, String universidad, String carrera) {
+        System.out.println("Estudiante: " + nombre + " | Edad: " + edad + " años");
+        System.out.println("Universidad: " + universidad + " | Carrera: " + carrera);
+    }
 }
