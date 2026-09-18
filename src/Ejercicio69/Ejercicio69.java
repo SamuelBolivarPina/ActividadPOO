258c76eabeecb34dca0429d12fca3918a918c11d
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 16:48:25 -0500

    importacion de carpetas para el ejercicio 69

--- src/Ejercicio69/Ejercicio69.java
+++ src/Ejercicio69/Ejercicio69.java
@@ -1,7 +1,7 @@
 package Ejercicio69;
-
 import Samuel.Samuel;
+import CanalStream.CanalStream;
 import NegocioGalletas.NegocioGalletas;
-
+import java.lang.reflect.Constructor;
 public class Ejercicio69 {
 }
