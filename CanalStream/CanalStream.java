fbf3ba8262f24fc4d77b707f175980fae6352325
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 16:09:11 -0500

    elaboracion de metodos de transmitir y mostrar estadisticas

--- CanalStream/CanalStream.java
+++ CanalStream/CanalStream.java
@@ -10,4 +10,12 @@ public class CanalStream {
         this.plataforma = plataforma;
         this.horasTransmitidas = horasTransmitidas;
     }
+    public void transmitir(double horas) {
+        if (horas > 0) {
+            this.horasTransmitidas += horas;
+        }
+    }
+    public void mostrarEstadisticas() {
+        System.out.println("Canal: " + nombreCanal + " | Plataforma: " + plataforma + " | Horas Emitidas: " + horasTransmitidas + " hrs");
+    }
 }
