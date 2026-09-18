19c4799f70781abfb4ee2b57e41a717db95b8548
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 16:12:37 -0500

    elaboracion de metodo de registro de venta

--- NegocioGalletas/NegocioGalletas.java
+++ NegocioGalletas/NegocioGalletas.java
@@ -2,4 +2,18 @@ public class NegocioGalletas {
     String nombreNegocio;
     String productoEstrella;
     double ventasDiarias;
+
+    public NegocioGalletas() {
+    }
+
+    public NegocioGalletas(String nombreNegocio, String productoEstrella, double ventasDiarias) {
+        this.nombreNegocio = nombreNegocio;
+        this.productoEstrella = productoEstrella;
+        this.ventasDiarias = ventasDiarias;
+    }
+    public void registrarVenta(double monto) {
+        if (monto > 0) {
+            this.ventasDiarias += monto;
+        }
+    }
 }
