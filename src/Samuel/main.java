a542530c804c940acd8bcd70a089501e5cf95859
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 17:13:26 -0500

    finalizacion de programacion de objetos de Samuel

--- src/Samuel/main.java
+++ src/Samuel/main.java
@@ -41,5 +41,29 @@ public class main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    System.out.println("=== SIMULACIÓN PROCEDIMENTAL: 5 Entidades Samuel ===");
+
+    String nombre1 = "Samuel", uni1 = "Universidad de Cartagena", carrera1 = "Ingeniería de Software"; int edad1 = 18;
+    String nombre2 = "Mateo", uni2 = "Universidad del Norte", carrera2 = "Ingeniería Industrial"; int edad2 = 19;
+    String nombre3 = "Lucas", uni3 = "Universidad Nacional", carrera3 = "Ingeniería Mecánica"; int edad3 = 20;
+    String nombre4 = "Daniel", uni4 = "Universidad de Antioquia", carrera4 = "Ingeniería Electrónica"; int edad4 = 18;
+    String nombre5 = "Camilo", uni5 = "Universidad del Valle", carrera5 = "Ingeniería Civil"; int edad5 = 21;
+
+        // Actualización procedimental
+        edad1 = cumplirAnioProc(edad1);
+
+        mostrarPerfilProc(nombre1, edad1, uni1, carrera1);
+        System.out.println("---");
+        mostrarPerfilProc(nombre2, edad2, uni2, carrera2);
+        System.out.println("---");
+        mostrarPerfilProc(nombre3, edad3, uni3, carrera3);
+        System.out.println("---");
+        mostrarPerfilProc(nombre4, edad4, uni4, carrera4);
+        System.out.println("---");
+        mostrarPerfilProc(nombre5, edad5, uni5, carrera5);
     }
 }
+
+
+
