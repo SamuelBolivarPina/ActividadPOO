275716c0c54daf5dc6521253e64dde70d79d89f2
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 17:21:34 -0500

    elaboracion de try para prevenir que se rompa el programa

--- src/NegocioGalletas/main.java
+++ src/NegocioGalletas/main.java
@@ -1,5 +1,7 @@
 package NegocioGalletas;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class main {
     // Métodos estáticos procedimentales
     public static double registrarVentaProc(double acumuladoActual, double monto) {
@@ -11,5 +13,24 @@ public class main {
 
     public static void mostrarResumenProc(String nombreNegocio, String productoEstrella, double ventasDiarias) {
         System.out.println("Negocio: " + nombreNegocio + " | Producto: " + productoEstrella + " | Ventas del Día: $" + ventasDiarias);
+    }
+
+    public static void main(String[] args) {
+        try {
+            // =========================================================
+            // 1. DEMOSTRACIÓN POO: 5 Objetos de NegocioGalletas (SIN 'new')
+            // =========================================================
+            System.out.println("=== 5 OBJETOS CLASE: NegocioGalletas (POO - Sin 'new') ===");
+
+            // Obtener el constructor de la clase NegocioGalletas desde el paquete
+            Constructor<NegocioGalletas> consNegocio = NegocioGalletas.class.getConstructor(String.class, String.class, double.class);
+
+            // Instanciación reflexiva de los 5 objetos individuales
+            NegocioGalletas n1 = consNegocio.newInstance("Galletas Artesanales", "Galleta de Chispas", 85000.0);
+            NegocioGalletas n2 = consNegocio.newInstance("Sweet Cookies", "Avena y Miel", 120000.0);
+            NegocioGalletas n3 = consNegocio.newInstance("Crispy Bakes", "Mantequilla", 60000.0);
+            NegocioGalletas n4 = consNegocio.newInstance("Don Cookie", "Rellenas de Nutella", 150000.0);
+            NegocioGalletas n5 = consNegocio.newInstance("Bakery Express", "Red Velvet", 95000.0);
+        }
     }
 }
